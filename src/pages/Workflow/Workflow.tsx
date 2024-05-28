7a0da5d830618f45173cf65f85918512fa7398e0
Om Shejul <contact@omshejul.com>
2024-05-28 11:37:34 +0530

    Refactor Workflow page and components, and add multi form functionality

--- src/pages/Workflow/Workflow.tsx
+++ src/pages/Workflow/Workflow.tsx
@@ -141,24 +141,8 @@ const DynamicForms: React.FC = () => {
         setWorkflowData(data)
       })
       .catch((error) => {
-        // console.error('Error fetching data:', error)
-        const data = {
-          Outlook_Reply: {
-            desc: 'null',
-            input: ['Start_token', 'Outlook_Reader_text', 'Outlook_Reader_sender_email', 'Outlook_Reader_sender', 'Sentiment_sentiment'],
-            output: ['Outlook_Reply_reply status', 'Outlook_Reply_ticket id', 'Outlook_Reply_refund', 'Outlook_Reply_sentiment', 'Outlook_Reply_appreciation'],
-          },
-          Simple_Function: {
-            desc: 'null',
-            input: ['Start_text', 'Start_lang', 'Start_client_id', 'Start_public_id'],
-            output: ['Start_text', 'Start_lang', 'Start_client_id', 'Start_public_id'],
-          },
-          WHATSAPP_SENDER: {
-            desc: 'null',
-            input: ['Start_url', 'Start_userid', 'Start_password', 'Start_to_number', 'Start_from_number', 'Dialog_Manager_message', 'Dialog_Manager_options'],
-            output: ['Dialog_Manager_message', 'Dialog_Manager_options'],
-          },
-        }
+        console.error('Error fetching data:', error)
+        const data = { "ASR": { "desc": "null", "input": ["Start_audio", "ASR_LANG_lang"], "output": ["ASR_text"] }, "ASR_LANG": { "desc": "null", "input": ["Start_audio"], "output": ["ASR_LANG_lang"] }, "Dialog_Manager": { "desc": "null", "input": ["Start_public_id", "WORD2NUM_text", "Start_client_id", "ASR_LANG_lang", "Start_client_id"], "output": ["Dialog_Manager_message", "Dialog_Manager_options", "Dialog_Manager_conversation_status"] }, "ELASTIC_SAVE": { "desc": "null", "input": ["Start_all_texts", "Start_index_name"], "output": ["ELASTIC_SAVE_response"] }, "ELASTIC_SEARCH": { "desc": "null", "input": ["Start_query", "Start_index_name"], "output": ["ELASTIC_SEARCH_ranked_results_long_text"] }, "Easynmt": { "desc": "null", "input": ["Start_text", "Start_target_lang"], "output": ["Easynmt_text"] }, "GmailReaderListExec": { "desc": "null", "input": ["Start_email_id", "Start_password"], "output": ["GmailReaderListExec_list_of_emails"] }, "Intent": { "desc": "null", "input": ["Outlook_Reader_text", "Start_c_id", "Start_lang"], "output": ["Intent_intent"] }, "Intent_Chatbot": { "desc": "null", "input": ["WR_ASR_text", "Start_lang", "Start_client_id", "Start_public_id"], "output": ["Intent_Chatbot_intent", "Intent_Chatbot_ambig_or_multi"] }, "NER": { "desc": "null", "input": ["Outlook_Reader_text", "Start_ner_ngram", "Start_c_id", "Start_lang"], "output": ["NER_ner", "NER_city"] }, "NER_Chatbot": { "desc": "null", "input": ["NER_NEW_text", "Start_lang", "Start_client_id", "Start_public_id", "Intent_Chatbot_intent", "Intent_Chatbot_ambig_or_multi"], "output": ["NER_Chatbot_ner"] }, "NER_NEW": { "desc": "null", "input": ["WORD2NUM_text", "Start_lang"], "output": ["NER_NEW_text"] }, "New_DM": { "desc": "null", "input": ["Start_public_id", "Start_text", "Start_lang", "Start_client_id"], "output": ["New_DM_message", "New_DM_options", "New_DM_conversation_status"] }, "OCR_ELASTIC": { "desc": "null", "input": ["Start_image_or_pdf"], "output": ["OCR_ELASTIC_ocr_text"] }, "Outlook_Mov": { "desc": "null", "input": ["Outlook_Reply_refund", "Outlook_Reply_sentiment", "Outlook_Reader_email_id", "Start_token"], "output": ["Outlook_Mov_subfolder_name"] }, "Outlook_Odb": { "desc": "null", "input": ["Start_command", "Start_data_to_insert", "Start_db_params"], "output": ["Outlook_Odb_status", "Outlook_Odb_description", "Outlook_Odb_rows"] }, "Outlook_Reader": { "desc": "null", "input": ["Start_token"], "output": ["Outlook_Reader_text", "Outlook_Reader_sender_email", "Outlook_Reader_sender", "Outlook_Reader_email_id"] }, "Outlook_Reply": { "desc": "null", "input": ["Start_token", "Outlook_Reader_text", "Outlook_Reader_sender_email", "Outlook_Reader_sender", "Sentiment_sentiment"], "output": ["Outlook_Reply_reply status", "Outlook_Reply_ticket id", "Outlook_Reply_refund", "Outlook_Reply_sentiment", "Outlook_Reply_appreciation"] }, "QUESTION_ANSWER": { "desc": "null", "input": ["Start_query", "ELASTIC_SEARCH_ranked_results_long_text"], "output": ["QUESTION_ANSWER_answer"] }, "SUMMARIZER": { "desc": "null", "input": ["ELASTIC_SEARCH_ranked_results_long_text", "Start_lang"], "output": ["SUMMARIZER_summary"] }, "Sentiment": { "desc": "null", "input": ["Outlook_Reader_text"], "output": ["Sentiment_sentiment", "Sentiment_score"] }, "Simple_Function": { "desc": "null", "input": ["Start_text", "Start_lang", "Start_client_id", "Start_public_id"], "output": ["Start_text", "Start_lang", "Start_client_id", "Start_public_id"] }, "TTS": { "desc": "null", "input": ["Start_message", "Start_lang"], "output": ["TTS_audio"] }, "Translation": { "desc": "null", "input": ["OCR_ELASTIC_ocr_text", "Start_source_lang", "Start_target_lang"], "output": ["Translation_text"] }, "Translation_dl": { "desc": "null", "input": ["ASR_text", "ASR_LANG_lang", "Start_target_lang"], "output": ["Translation_dl_text"] }, "WHATSAPP_SENDER": { "desc": "null", "input": ["Start_url", "Start_userid", "Start_password", "Start_to_number", "Start_from_number", "Dialog_Manager_message", "Dialog_Manager_options"], "output": ["Dialog_Manager_message", "Dialog_Manager_options"] }, "WORD2NUM": { "desc": "null", "input": ["WR_ASR_text", "Start_lang"], "output": ["WORD2NUM_text"] }, "WR_ASR": { "desc": "null", "input": ["Start_audio", "Start_lang"], "output": ["WR_ASR_text"] }, "hdfc_conv": { "desc": "null", "input": ["WR_ASR_text", "ASR_LANG_lang", "Start_client_id"], "output": ["hdfc_conv_message"] } }
         const keys = Object.keys(data)
         setDropdownOptions(keys)
         setWorkflowData(data)
@@ -189,23 +173,19 @@ const DynamicForms: React.FC = () => {
   const handleFormChange = (index: number) => ({ data }: { data: any }) => {
     const updatedFormDataArray = [...formDataArray]
     updatedFormDataArray[index] = data
-
-    // Create a copy of the schema array
     const updatedSchemaArray = [...schemaArray]
 
     if (index === 0) {
       const allInputs = Object.values(workflowData)
         .flatMap(workflow => workflow.input)
-        .filter((value, idx, self) => self.indexOf(value) === idx) 
-  
-      // Filter outputs that start with 'Start_'
+        .filter((value, idx, self) => self.indexOf(value) === idx)
       const startOutputs = Object.values(workflowData)
         .flatMap(workflow => workflow.output)
         .filter(output => output.startsWith('Start_'))
-        .filter((value, idx, self) => self.indexOf(value) === idx) 
-  
-      updatedFormDataArray[index].output = startOutputs as any;
-  
+        .filter((value, idx, self) => self.indexOf(value) === idx)
+
+      updatedFormDataArray[index].output = startOutputs as any
+
       updatedSchemaArray[index] = {
         ...schemaArray[index],
         properties: {
@@ -235,55 +215,49 @@ const DynamicForms: React.FC = () => {
         },
       }
     } else {
-    const selectedNextValues = formDataArray.slice(0, index).map(form => form.next)
-    let remainingOptions = dropdownOptions.filter(option => !selectedNextValues.includes(option))
-    remainingOptions = remainingOptions.length === 0 ? ['NONE'] : remainingOptions
+      const selectedNextValues = formDataArray.slice(0, index).map(form => form.next)
+      let remainingOptions = dropdownOptions.filter(option => !selectedNextValues.includes(option))
+      remainingOptions = remainingOptions.length === 0 ? ['NONE'] : remainingOptions
 
-    const selectedType = formDataArray[index - 1]?.next || ''
-    const previousOutputs = formDataArray[index - 1]?.output || []
-    const typeInputs = workflowData[selectedType]?.input || ['']
-    const typeOutputs = workflowData[selectedType]?.output || ['']
+      const selectedType = formDataArray[index - 1]?.next || ''
+      const previousOutputs = formDataArray[index - 1]?.output || []
+      const typeInputs = workflowData[selectedType]?.input || ['']
+      const typeOutputs = workflowData[selectedType]?.output || ['']
 
-    // Combine the previous node's outputs with current type inputs
-    const combinedInputs = Array.from(new Set([...previousOutputs, ...typeInputs]))
+      const combinedInputs = Array.from(new Set([...previousOutputs, ...typeInputs]))
 
-    updatedSchemaArray[index] = {
-      ...schemaArray[index],
-      properties: {
-        ...schemaArray[index].properties,
-        type: {
-          ...schemaArray[index].properties.type,
-          enum: [selectedType],
-        },
-        next: {
-          ...schemaArray[index].properties.next,
-          enum: remainingOptions,
-        },
-        input: {
-          ...schemaArray[index].properties.input,
-          items: {
-            type: 'string',
-            enum: combinedInputs.length > 0 ? combinedInputs : [''],
+      updatedSchemaArray[index] = {
+        ...schemaArray[index],
+        properties: {
+          ...schemaArray[index].properties,
+          type: {
+            ...schemaArray[index].properties.type,
+            enum: [selectedType],
+          },
+          next: {
+            ...schemaArray[index].properties.next,
+            enum: remainingOptions,
+          },
+          input: {
+            ...schemaArray[index].properties.input,
+            items: {
+              type: 'string',
+              enum: combinedInputs.length > 0 ? combinedInputs : [''],
+            },
+          },
+          output: {
+            ...schemaArray[index].properties.output,
+            items: {
+              type: 'string',
+              enum: typeOutputs.length > 0 ? typeOutputs : [''],
+            },
           },
         },
-        output: {
-          ...schemaArray[index].properties.output,
-          items: {
-            type: 'string',
-            enum: typeOutputs.length > 0 ? typeOutputs : [''],
-          },
-        },
-      },
+      }
     }
-  }
 
     setSchemaArray(updatedSchemaArray)
     setFormDataArray(updatedFormDataArray)
-  }
-
-
-  const generateRandomHex = () => {
-    return Math.floor(Math.random() * 0xFFFFFFFF).toString(16).padStart(8, '0')
   }
 
   const deleteForm = (index: number) => {
